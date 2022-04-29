88c14411cf4d489a86887907a9e4e1d6c70cc903
evolution-project <evolution-project@protonmail.com>
2022-04-29 14:05:38 +0300

    ups

--- src/currency_core/currency_config.h
+++ src/currency_core/currency_config.h
@@ -239,13 +239,13 @@
 #define BLOCK_MINOR_VERSION_GENESIS                     0
 #define BLOCK_MAJOR_VERSION_INITIAL                     0
 #ifndef TESTNET
-#define ZANO_HARDFORK_01_AFTER_HEIGHT                   1440    //was 194624
-#define ZANO_HARDFORK_02_AFTER_HEIGHT                   1800    //was 999999
-#define ZANO_HARDFORK_03_AFTER_HEIGHT                   1801    //was 1082577
+#define EVOX_HARDFORK_01_AFTER_HEIGHT                   1440    //was 194624
+#define EVOX_HARDFORK_02_AFTER_HEIGHT                   1800    //was 999999
+#define EVOX_HARDFORK_03_AFTER_HEIGHT                   1801    //was 1082577
 #else
-#define ZANO_HARDFORK_01_AFTER_HEIGHT                   1440
-#define ZANO_HARDFORK_02_AFTER_HEIGHT                   1800
-#define ZANO_HARDFORK_03_AFTER_HEIGHT                   1801
+#define EVOX_HARDFORK_01_AFTER_HEIGHT                   1440
+#define EVOX_HARDFORK_02_AFTER_HEIGHT                   1800
+#define EVOX_HARDFORK_03_AFTER_HEIGHT                   1801
 #endif
 
 
