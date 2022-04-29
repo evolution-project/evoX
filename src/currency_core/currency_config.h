87ceb6e7c848fad76e8e69ccc16a4048047e27cc
evolution-project <evolution-project@protonmail.com>
2022-04-29 13:24:09 +0300

    HF set

--- src/currency_core/currency_config.h
+++ src/currency_core/currency_config.h
@@ -239,9 +239,9 @@
 #define BLOCK_MINOR_VERSION_GENESIS                     0
 #define BLOCK_MAJOR_VERSION_INITIAL                     0
 #ifndef TESTNET
-#define ZANO_HARDFORK_01_AFTER_HEIGHT                   194624
-#define ZANO_HARDFORK_02_AFTER_HEIGHT                   999999
-#define ZANO_HARDFORK_03_AFTER_HEIGHT                   1082577
+#define ZANO_HARDFORK_01_AFTER_HEIGHT                   1440    //was 194624
+#define ZANO_HARDFORK_02_AFTER_HEIGHT                   1800    //was 999999
+#define ZANO_HARDFORK_03_AFTER_HEIGHT                   1801    //was 1082577
 #else
 #define ZANO_HARDFORK_01_AFTER_HEIGHT                   1440
 #define ZANO_HARDFORK_02_AFTER_HEIGHT                   1800
