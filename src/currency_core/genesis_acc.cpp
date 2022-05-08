7ceaf6ee43270540454869922ae86e20751fd5ff
evolution-project <evolution-project@protonmail.com>
2022-05-08 17:20:32 +0300

    ggenesis_tx_pub_key_str same as gdefault_genesis

--- src/currency_core/genesis_acc.cpp
+++ src/currency_core/genesis_acc.cpp
@@ -10,7 +10,7 @@
 namespace currency
 {
 #ifndef TESTNET
-  const std::string ggenesis_tx_pub_key_str = "1368ee72157e79c380f53682f63358a92a6ebf0aa896c73a6c81e3239b120ab0";
+  const std::string ggenesis_tx_pub_key_str = "bf6440a6a794a8bfa1e5a4c73fa0eeb16e461dd85280d6c7a73bbd22f0f69d58";
   const crypto::public_key ggenesis_tx_pub_key = epee::string_tools::parse_tpod_from_hex_string<crypto::public_key>(ggenesis_tx_pub_key_str);
   const genesis_tx_dictionary_entry ggenesis_dict[26] = {
     { 898363347618325980ULL,7 },
@@ -41,7 +41,7 @@ namespace currency
     { 17472133472787764818ULL,10 }
   };
 #else 
-  const std::string ggenesis_tx_pub_key_str    = "1368ee72157e79c380f53682f63358a92a6ebf0aa896c73a6c81e3239b120ab0";
+  const std::string ggenesis_tx_pub_key_str    = "bf6440a6a794a8bfa1e5a4c73fa0eeb16e461dd85280d6c7a73bbd22f0f69d58";
   const crypto::public_key ggenesis_tx_pub_key = epee::string_tools::parse_tpod_from_hex_string<crypto::public_key>(ggenesis_tx_pub_key_str);
   const genesis_tx_dictionary_entry ggenesis_dict[5] = {
     { 4413532107669521528ULL, 2 },
